f2317c55ebcac02fae05a12e91b20b5e66ea01d2
Wildan <wildanwigenta52@gmail.com>
2025-07-10 22:58:35 +0700

    add taskform livewire component

---
+++ app/Livewire/TaskForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TaskForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.task-form');
+    }
+}
