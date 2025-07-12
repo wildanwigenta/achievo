d5f1c0f4a81c8196485d7d1f7c5217265ab8be86
Wildan <wildanwigenta52@gmail.com>
2025-07-13 01:21:21 +0700

    update tasklist view

--- resources/views/livewire/task-list.blade.php
+++ resources/views/livewire/task-list.blade.php
@@ -1,3 +1,10 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    @foreach ($tasks as $task)
+    <div>
+        <h4>{{ $task->title }} ({{ $task->priority }})</h4>
+        <p>{{ $task->description }}</p>
+        <p>Deadline: {{ $task->due_date }}</p>
+        <button wire:click="delete({{ $task->id }})">Hapus</button>
+    </div>
+    @endforeach
 </div>
