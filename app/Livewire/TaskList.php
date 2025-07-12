d021424ef4d775cddda6a5734268617bc20f4206
Wildan <wildanwigenta52@gmail.com>
2025-07-13 01:20:26 +0700

    update tasklist (livewire)

--- app/Livewire/TaskList.php
+++ app/Livewire/TaskList.php
@@ -1,11 +1,26 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\Task;
 use Livewire\Component;
 
 class TaskList extends Component
 {
+    public $tasks;
+
+    protected $listeners = ['taskUpdated' => 'mount'];
+
+    public function mount()
+    {
+        $this->tasks = Task::orderBy('order')->get();
+    }
+
+    public function delete($id)
+    {
+        Task::find($id)->delete();
+        $this->mount();
+    }
+
     public function render()
     {
         return view('livewire.task-list');
