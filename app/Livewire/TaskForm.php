7cc901a42d6a02ddc79727b33e8312cf832cb76c
Wildan <wildanwigenta52@gmail.com>
2025-07-12 19:15:51 +0700

    update taskform (livewire)

--- app/Livewire/TaskForm.php
+++ app/Livewire/TaskForm.php
@@ -1,11 +1,34 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\Task;
 use Livewire\Component;
 
 class TaskForm extends Component
 {
+    public $title, $description, $due_date, $priority, $category;
+
+    public function store()
+    {
+        $this->validate([
+            'title' => 'required',
+            'due_date' => 'required|date',
+            'priority' => 'required',
+        ]);
+
+        Task::create([
+            'title' => $this->title,
+            'description' => $this->description,
+            'due_date' => $this->due_date,
+            'priority' => $this->priority,
+            'category' => $this->category,
+        ]);
+
+        session()->flash('success', 'Tugas berhasil ditambahkan');
+        $this->reset();
+        $this->emit('taskUpdated');
+    }
+
     public function render()
     {
         return view('livewire.task-form');
