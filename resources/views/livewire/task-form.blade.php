62f82c7a4a267492656d508690696aba7dede89f
Wildan <wildanwigenta52@gmail.com>
2025-07-13 01:19:01 +0700

    update taskform view

--- resources/views/livewire/task-form.blade.php
+++ resources/views/livewire/task-form.blade.php
@@ -1,3 +1,15 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <form wire:submit.prevent="store">
+    <input type="text" wire:model="title" placeholder="Judul">
+    <textarea wire:model="description" placeholder="Deskripsi"></textarea>
+    <input type="datetime-local" wire:model="due_date">
+    <select wire:model="priority">
+        <option value="">Pilih Prioritas</option>
+        <option value="low">Rendah</option>
+        <option value="medium">Sedang</option>
+        <option value="high">Tinggi</option>
+    </select>
+    <input type="text" wire:model="category" placeholder="Kategori">
+    <button type="submit">Simpan</button>
+</form>
 </div>
