9d14819e02421037c441f6ea94750dbfefa5123f
Wildan <wildanwigenta52@gmail.com>
2025-07-13 22:57:51 +0700

    update task models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'priority',
+        'category',
+        'status',
+        'order',
+    ];
 }
