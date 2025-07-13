420228cb22cdecaa1ea573399ac5c4e066d231f6
Wildan <wildanwigenta52@gmail.com>
2025-07-14 00:27:47 +0700

    update taskform

--- resources/views/livewire/task-form.blade.php
+++ resources/views/livewire/task-form.blade.php
@@ -9,15 +9,15 @@
         <label for="">Judul Tugas:</label>
         <input type="text" wire:model="title" class="w-full border px-3 py-2 rounded">
         @error('title') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
-
+        <br>
         <label for="">Deskripsi Tugas:</label>
         <textarea wire:model="description" placeholder="Deskripsi (Opsional)" class="w-full border px-3 py-2 rounded"></textarea>
         @error('description') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
-
+        <br>
         <label for="">Tanggal Jatuh Tempo:</label>
         <input type="datetime-local" wire:model="due_date" class="w-full border px-3 py-2 rounded" >
         @error('due_date') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
-
+        <br>
         <label for="">Prioritas:</label>
         <select wire:model="priority" class="w-full border px-3 py-2 rounded">
             <option value="">Pilih Prioritas</option>
@@ -26,11 +26,11 @@
             <option value="high">Tinggi</option>
         </select>
         @error('priority') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
-
+        <br>
         <label for="">Kategori:</label>
         <input type="text" wire:model="category" placeholder="Kategori (Opsional)" class="w-full border px-3 py-2 rounded">
         @error('category') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
-
+        
         <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Tambah Tugas</button>
     </form>
 </div>
