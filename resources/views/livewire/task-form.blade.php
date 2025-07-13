0893bc55c3c11f23a0a4783716cecf83e17a63dd
Wildan <wildanwigenta52@gmail.com>
2025-07-13 23:45:03 +0700

    update taskform

--- resources/views/livewire/task-form.blade.php
+++ resources/views/livewire/task-form.blade.php
@@ -6,15 +6,19 @@
     @endif
 
     <form wire:submit.prevent="store" class="space-y-3">
-        <input type="text" wire:model="title" placeholder="Judul Tugas" class="w-full border px-3 py-2 rounded">
+        <label for="">Judul Tugas:</label>
+        <input type="text" wire:model="title" class="w-full border px-3 py-2 rounded">
         @error('title') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
 
+        <label for="">Deskripsi Tugas:</label>
         <textarea wire:model="description" placeholder="Deskripsi (Opsional)" class="w-full border px-3 py-2 rounded"></textarea>
         @error('description') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
 
-        <input type="datetime-local" wire:model="due_date" class="w-full border px-3 py-2 rounded" placeholder="tenggat waktu">
+        <label for="">Tanggal Jatuh Tempo:</label>
+        <input type="datetime-local" wire:model="due_date" class="w-full border px-3 py-2 rounded" >
         @error('due_date') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
 
+        <label for="">Prioritas:</label>
         <select wire:model="priority" class="w-full border px-3 py-2 rounded">
             <option value="">Pilih Prioritas</option>
             <option value="low">Rendah</option>
@@ -23,6 +27,7 @@
         </select>
         @error('priority') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
 
+        <label for="">Kategori:</label>
         <input type="text" wire:model="category" placeholder="Kategori (Opsional)" class="w-full border px-3 py-2 rounded">
         @error('category') <span class="text-red-600 text-sm">{{ $message }}</span> @enderror
 
