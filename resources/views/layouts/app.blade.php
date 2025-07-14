b2128454aab20bec3d26a41ef4134b22b38a8bcd
Wildan <wildanwigenta52@gmail.com>
2025-07-14 22:50:32 +0700

    update app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title> Achievo </title>
+    <title>Achievo</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-<body class="bg-gray-100 min-h-screen flex justify-center items-start p-6">
-    <div class="w-full max-w-2xl bg-white shadow-md rounded-lg p-6">
-        {{-- <h1 class="text-2xl font-bold mb-6">Todo List</h1> --}}
+<body class="bg-gradient-to-br from-blue-100 via-white to-purple-100 min-h-screen flex justify-center items-start p-6">
+    <div class="w-full max-w-3xl bg-white shadow-2xl rounded-2xl p-8">
         @yield('content')
     </div>
     @livewireScripts
