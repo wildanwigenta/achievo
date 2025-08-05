cccade166a30ec48b3fbafc817d99c5ba72e86fd
Wildan <wildanwigenta52@gmail.com>
2025-08-05 12:44:28 +0700

    add section

--- resources/views/tasks/edit_tugas.blade.php
+++ resources/views/tasks/edit_tugas.blade.php
@@ -4,3 +4,11 @@
     <h1 class="text-3xl font-bold text-gray-800 mb-6"> Edit Tugas</h1>
     @livewire('task-edit', ['id' => $id])
 @endsection
+@section('scripts')
+    <script>
+        document.addEventListener('livewire:load', function () {
+            Livewire.on('taskUpdated', () => {
+                alert('Tugas berhasil diperbarui!');
+            });
+        });
+    </script>
