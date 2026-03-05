87cf6b6070628204a5860de1945c7cc4f2e628e4
Wildan <wildanwigenta52@gmail.com>
2026-03-05 23:25:30 +0700

    feat: tambah tugas

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,7 @@
     <h1 class="text-3xl font-bold text-gray-800 mb-6"> Daftar Tugas</h1>
     <a href="{{ route('tasks.add_tugas') }}"
         class="mb-4 inline-block px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded hover:bg-blue-700 transition">
-        + Tambah Tugas
+        Tambah Tugas
     </a>
     @livewire('task-list')
 @endsection
